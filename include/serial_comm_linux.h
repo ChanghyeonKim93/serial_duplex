39b04fac2c3126041a23bf3284bbe077240725a7
ChanghyeonKim93 <hyun91015@gmail.com>
2022-06-22 21:52:58 +0900

    tx init.

--- include/serial_comm_linux.h
+++ include/serial_comm_linux.h
@@ -8,6 +8,7 @@
 #include <thread>
 #include <string>
 #include <cstring>
+#include <chrono>
 
 // serial
 #include <stdio.h>
@@ -18,6 +19,8 @@
 #include <fcntl.h>
 #include <sys/poll.h>
 #include <sys/time.h>
+
+using namespace std::chrono_literals;
 
 class SerialCommunicatorLinux {
 public:
@@ -32,19 +35,13 @@ public:
         seq_send_  = 0;
         
         // Check whether this baud rate is valid
-        if(baud_rate == 57600) BAUD_RATE_ = B57600;
-        else if(baud_rate == 57600) BAUD_RATE_ = B57600;
-        else if(baud_rate == 57600) BAUD_RATE_ = B57600;
-        else if(baud_rate == 57600) BAUD_RATE_ = B57600;
-        else if(baud_rate == 57600) BAUD_RATE_ = B57600;
-        else if(baud_rate == 57600) BAUD_RATE_ = B57600;
-        else if(baud_rate == 57600) BAUD_RATE_ = B57600;
-        else if(baud_rate == 115200) BAUD_RATE_ =  B115200;
-        else if(baud_rate == 230400) BAUD_RATE_ =  B230400;
-        else if(baud_rate == 460800) BAUD_RATE_ =  B460800;
-        else if(baud_rate == 500000) BAUD_RATE_ =  B500000;
-        else if(baud_rate == 576000) BAUD_RATE_ =  B576000;
-        else if(baud_rate == 921600) BAUD_RATE_ =  B921600;
+        if(     baud_rate == 57600)   BAUD_RATE_ = B57600;
+        else if(baud_rate == 115200)  BAUD_RATE_ = B115200;
+        else if(baud_rate == 230400)  BAUD_RATE_ = B230400;
+        else if(baud_rate == 460800)  BAUD_RATE_ = B460800;
+        else if(baud_rate == 500000)  BAUD_RATE_ = B500000;
+        else if(baud_rate == 576000)  BAUD_RATE_ = B576000;
+        else if(baud_rate == 921600)  BAUD_RATE_ = B921600;
         else if(baud_rate == 1000000) BAUD_RATE_ = B1000000;
         else if(baud_rate == 1152000) BAUD_RATE_ = B1152000;
         else if(baud_rate == 1500000) BAUD_RATE_ = B1500000;
@@ -72,19 +69,27 @@ public:
         // #define  B4000000  0010017
         // #define __MAX_BAUD B4000000
 
-        this->runThread();
+        openSerialPort();
+        
+        this->runThreadRX();
+        this->runThreadTX();
 
         printf("[SerialComm] Portname is {%s}, message length: {%d}\n", portname_.c_str(), MSG_LEN_);
     };
 
     // deconstructor
     ~SerialCommunicatorLinux() {
-        this->thread_.join();
-    }
-
-    void runThread(){
-        this->thread_ = std::thread(&SerialCommunicatorLinux::process, this);
+        if(thread_rx_.joinable()) this->thread_rx_.join();
+        if(thread_tx_.joinable()) this->thread_tx_.join();
     };
+
+    void runThreadRX(){
+        this->thread_rx_ = std::thread(&SerialCommunicatorLinux::processRX, this);
+    };
+
+    void runThreadTX(){
+        this->thread_tx_ = std::thread(&SerialCommunicatorLinux::processTX, this);
+    }
 
 
 private:
@@ -92,15 +97,29 @@ private:
        
     };
 
-    void process(){
-        printf("Process runs.\n");
+    void send_withChecksum(const char* data, int len){
+        char crc = stringChecksum(data,0,len-1);
+
+        char len_c = (char)len;
+        write(fd_,&STX_,  1);
+        write(fd_,&len_c, 1);
+        write(fd_,data,   len);
+        write(fd_,&crc,   1);
+        write(fd_,&ETX_,  1);
+    };
+
+    void openSerialPort(){
+        printf("Opening the serial port...\n");
         fd_ = open(portname_.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
         if (fd_ == -1) {
-            printf("Error - no serial port {%s} is opened! Check the serial device.\n", portname_.c_str());
-            return;
+            throw std::runtime_error("Error - no serial port '" + portname_ + "' is opened! Check the serial device.\n");
         }
-        else printf("{%s} is opened.\n",portname_.c_str());
+        else {
+            printf("{%s} is opened.\n",portname_.c_str());
+        }
+    };
 
+    void processRX(){
         // newtio_rx initialization.
         memset(&this->newtio_rx_, 0, sizeof(this->newtio_rx_));
         newtio_rx_.c_cflag     = BAUD_RATE_ | CS8 | CLOCAL | CREAD;
@@ -138,7 +157,6 @@ private:
                     // char msg_recv[1024];
                     // this->read_withChecksum(msg_recv);
                     int len_read = read(fd_, buf_, BUF_SIZE);
-                    
                     for(int i = 0; i < len_read; ++i) {
                         if(buf_[i] == ETX_){ // Test 1: ETX
                             // data part : serial_stack_[1] ~ serial_stack_[MSG_LEN_]
@@ -187,8 +205,18 @@ private:
         } // END WHILE
     };
 
+    void processTX(){
+        int cnt = 0;
+        while(true){
+            char data[15]={'a','b','c','d','e','a','b','c','d','e','a','b','c','d','e'};
+            send_withChecksum(data, 15);
+            std::cout << " TX OK!:"<< ++cnt << std::endl;
+            std::this_thread::sleep_for(50ms);
+        }
+    };
+
 private:
-    char stringChecksum(char* s, int idx_start, int idx_end) {
+    char stringChecksum(const char* s, int idx_start, int idx_end) {
         char c = 0;
         for(int i = idx_start; i <= idx_end; i++) c ^= s[i];
         return c;
@@ -198,8 +226,11 @@ private:
     std::string portname_;
     int BAUD_RATE_;
 
-    int fd_;
+    int fd_; // file descriptor...
+    
     struct termios newtio_rx_;
+    struct termios newtio_tx_;
+
     struct pollfd poll_events_;
     int poll_state_;
     char buf_[BUF_SIZE];
@@ -216,12 +247,13 @@ private:
     uint64_t seq_send_;
 
 private:
-    std::thread thread_;
+    std::thread thread_rx_;
+    std::thread thread_tx_;
 
 private:
 
     inline short decode2BytesToShort(char h_byte, char l_byte){
-        return (short) ( (unsigned char)h_byte << 8 | (unsigned char)l_byte);
+        return (short) (((unsigned char)h_byte << 8 )| (unsigned char)l_byte);
     };
 
     double decodeTime(char sec_l, char sec_h, char usec0, char usec1, char usec2, char usec3){
